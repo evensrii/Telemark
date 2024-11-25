0959c7a3ca6b371c8e85c0d8aeeb1b98eaad5e09
Even Sannes Riiser <even.s.riiser@gmail.com>
2024-11-25 17:48:31 +0100

    sf

--- Python/Queries/09_Innvadrere_og_inkludering/Utdanning/innv_fullført_vgo.py
+++ Python/Queries/09_Innvadrere_og_inkludering/Utdanning/innv_fullført_vgo.py
@@ -1,22 +1,33 @@
 import requests
-from io import BytesIO
-from io import StringIO
-import numpy as np
-import pandas as pd
-import datetime as dt
 import sys
 import os
 import glob
-import dtale
+from io import BytesIO
+from io import StringIO
+import pandas as pd
 from pyjstat import pyjstat
+
+# Import the utility functions from the Helper_scripts folder
+from Helper_scripts.utility_functions import fetch_data
+from Helper_scripts.utility_functions import delete_files_in_temp_folder
+from Helper_scripts.email_functions import notify_errors
+from Helper_scripts.github_functions import upload_github_file
+from Helper_scripts.github_functions import download_github_file
+from Helper_scripts.github_functions import compare_to_github
+
+# Capture the name of the current script
+script_name = os.path.basename(__file__)
+
+# Example list of error messages to collect errors during execution <--- Eksempel på liste for å samle feilmeldinger under kjøring
+error_messages = []
 
 ################# Fullført - Innvandrere #################
 
 # Endepunkt for SSB API
-POST_URL = "https://data.ssb.no/api/v0/no/table/13628/"
+POST_URL_innv = "https://data.ssb.no/api/v0/no/table/13628/"
 
 # Spørring for å hente ut data fra SSB
-payload = {
+payload_innv = {
     "query": [
         {"code": "Region", "selection": {"filter": "item", "values": ["08"]}},
         {
@@ -33,15 +44,25 @@ payload = {
     "response": {"format": "json-stat2"},
 }
 
-resultat = requests.post(POST_URL, json=payload)
+## Kjøre spørringer i try-except for å fange opp feil. Quitter hvis feil.
 
-if resultat.status_code == 200:
-    print("Spørring ok")
-else:
-    print(f"Spørring feilet. Statuskode: {resultat.status_code}")
+try:
+    df_innvandrere = fetch_data(
+        url=POST_URL_innv,
+        payload=payload_innv,  # The JSON payload for POST requests. If None, a GET request is used.
+        error_messages=error_messages,
+        query_name="Innvandrere fullført VGO",
+        response_type="json",  # The expected response type, either 'json' or 'csv'.
+        # delimiter=";", # The delimiter for CSV data (default: ';').
+        # encoding="ISO-8859-1", # The encoding for CSV data (default: 'ISO-8859-1').
+    )
+except Exception as e:
+    print(f"Error occurred: {e}")
+    notify_errors(error_messages, script_name=script_name)
+    raise RuntimeError(
+        "A critical error occurred during data fetching, stopping execution."
+    )
 
-dataset = pyjstat.Dataset.read(resultat.text)
-df_innvandrere = dataset.write("dataframe")
 df_innvandrere.info()
 df_innvandrere.head()
 
@@ -65,10 +86,10 @@ df_innvandrere["gruppe"] = "Innvandrere"
 ################# Fullført - Hele befolkningen #################
 
 # Endepunkt for SSB API
-POST_URL = "https://data.ssb.no/api/v0/no/table/12971/"
+POST_URL_hele_bef = "https://data.ssb.no/api/v0/no/table/12971/"
 
 # Spørring for å hente ut data fra SSB
-payload = {
+payload_hele = {
     "query": [
         {"code": "Region", "selection": {"filter": "item", "values": ["08"]}},
         {
@@ -85,15 +106,25 @@ payload = {
     "response": {"format": "json-stat2"},
 }
 
-resultat = requests.post(POST_URL, json=payload)
+## Kjøre spørringer i try-except for å fange opp feil. Quitter hvis feil.
 
-if resultat.status_code == 200:
-    print("Spørring ok")
-else:
-    print(f"Spørring feilet. Statuskode: {resultat.status_code}")
+try:
+    df_befolkningen = fetch_data(
+        url=POST_URL_hele_bef,
+        payload=payload_hele,  # The JSON payload for POST requests. If None, a GET request is used.
+        error_messages=error_messages,
+        query_name="Hele befolkningen fullført VGO",
+        response_type="json",  # The expected response type, either 'json' or 'csv'.
+        # delimiter=";", # The delimiter for CSV data (default: ';').
+        # encoding="ISO-8859-1", # The encoding for CSV data (default: 'ISO-8859-1').
+    )
+except Exception as e:
+    print(f"Error occurred: {e}")
+    notify_errors(error_messages, script_name=script_name)
+    raise RuntimeError(
+        "A critical error occurred during data fetching, stopping execution."
+    )
 
-dataset = pyjstat.Dataset.read(resultat.text)
-df_befolkningen = dataset.write("dataframe")
 df_befolkningen.info()
 df_befolkningen.head()
 # dtale.show(df_befolkningen, open_browser=True)
@@ -144,54 +175,16 @@ df_telemark_pivot = df_telemark.pivot_table(
 df_telemark_pivot.columns = [col for col in df_telemark_pivot.columns]
 
 
-#### Save df as a csv file
+##################### Lagre til csv, sammenlikne og eventuell opplasting til Github #####################
 
-# Ønsket filnavn <----------- MÅ ENDRES MANUELT!
-csv_file_name = f"fullført_vgo_innv_befolk.csv"
-df_telemark_pivot.to_csv(
-    (f"../../Temp/{csv_file_name}"), index=False
-)  # Relativt til dette scriptet.
+file_name = "fullført_vgo_innv_befolk.csv"
+github_folder = "Data/09_Innvandrere og inkludering/Utdanning"
+temp_folder = os.environ.get("TEMP_FOLDER")
 
-##################### Opplasting til Github #####################
+compare_to_github(
+    df_telemark_pivot, file_name, github_folder, temp_folder
+)  # <--- Endre navn på dataframe her!
 
-# Legge til directory hvor man finner github_functions.py i sys.path for å kunne importere denne
-current_directory = os.path.dirname(os.path.abspath(__file__))
-two_levels_up_directory = os.path.abspath(
-    os.path.join(current_directory, os.pardir, os.pardir)
-)
-sys.path.append(two_levels_up_directory)
+##################### Remove temporary local files #####################
 
-from github_functions import upload_file_to_github
-
-# Hvis eksisterer, oppdater filen. Hvis ikke, opprett filen.
-
-csv_file = f"../../Temp/{csv_file_name}"
-destination_folder = "Data/09_Innvandrere og inkludering/Utdanningsnivå Telemark"  # Mapper som ikke eksisterer vil opprettes automatisk.
-github_repo = "evensrii/Telemark"
-git_branch = "main"
-
-upload_file_to_github(csv_file, destination_folder, github_repo, git_branch)
-
-##################### Remove temporary files #####################
-
-# Delete files in folder using glob
-
-
-def delete_files_in_folder(folder_path):
-    # Construct the path pattern to match all files in the folder
-    files = glob.glob(os.path.join(folder_path, "*"))
-
-    # Iterate over the list of files and delete each one
-    for file_path in files:
-        try:
-            os.remove(file_path)
-            print(f"Deleted file: {file_path}")
-        except Exception as e:
-            print(f"Error deleting file {file_path}: {e}")
-
-
-# Specify the folder path
-folder_path = "../../Temp"
-
-# Call the function to delete files
-delete_files_in_folder(folder_path)
+delete_files_in_temp_folder()
