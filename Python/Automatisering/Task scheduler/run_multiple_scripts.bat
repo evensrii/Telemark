97171ce87bc9eb992bca9c8828ceb6bbe2fff55c
Even Sannes Riiser <even.s.riiser@gmail.com>
2024-11-28 14:50:58 +0100

    sdf

--- Python/Automatisering/Task scheduler/run_multiple_scripts.bat
+++ Python/Automatisering/Task scheduler/run_multiple_scripts.bat
@@ -7,11 +7,11 @@ REM Activate the conda environment
 CALL conda activate analyse
 
 REM Run the first Python script
-python "C:\Users\_eve1509\Documents\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_innvandrere.py" >> ./logs/log_sysselsatte.txt 2>&1
+python "C:\Users\_eve1509\Documents\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_innvandrere.py" > ./logs/log_sysselsatte.txt 2>&1
 IF %ERRORLEVEL% NEQ 0 echo "Script1.py failed with error code %ERRORLEVEL%"
 
 REM Run the second Python script
-python "C:\Users\_eve1509\Documents\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Innvandrerbefolkningen\botid.py" >> ./logs/log_botid.txt 2>&1
+python "C:\Users\_eve1509\Documents\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Innvandrerbefolkningen\botid.py" > ./logs/log_botid.txt 2>&1
 IF %ERRORLEVEL% NEQ 0 echo "Script2.py failed with error code %ERRORLEVEL%"
 
 REM Log completion
