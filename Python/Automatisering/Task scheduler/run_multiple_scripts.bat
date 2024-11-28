99133db98266a66e30e4dddc90175dc92b411a27
Even Sannes Riiser <even.s.riiser@gmail.com>
2024-11-28 13:55:17 +0100

    asd

--- Python/Automatisering/Task scheduler/run_multiple_scripts.bat
+++ Python/Automatisering/Task scheduler/run_multiple_scripts.bat
@@ -1,17 +1,17 @@
 @echo off
 
 REM Ensure conda is initialized
-CALL C:\ProgramData\anaconda3\Scripts\activate.bat
+CALL C:\Users\_eve1509\AppData\Local\anaconda3\Scripts\activate.bat
 
 REM Activate the conda environment
 CALL conda activate analyse
 
 REM Run the first Python script
-python "C:\Users\eve1509\OneDrive - Telemark fylkeskommune\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_innvandrere.py" >> ./logs/log_sysselsatte.txt 2>&1
+python "C:\Users\_eve1509\Documents\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_innvandrere.py" >> ./logs/log_sysselsatte.txt 2>&1
 IF %ERRORLEVEL% NEQ 0 echo "Script1.py failed with error code %ERRORLEVEL%"
 
 REM Run the second Python script
-python "C:\Users\eve1509\OneDrive - Telemark fylkeskommune\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Innvandrerbefolkningen\botid.py" >> ./logs/log_botid.txt 2>&1
+python "C:\Users\_eve1509\Documents\Github\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Innvandrerbefolkningen\botid.py" >> ./logs/log_botid.txt 2>&1
 IF %ERRORLEVEL% NEQ 0 echo "Script2.py failed with error code %ERRORLEVEL%"
 
 REM Log completion
