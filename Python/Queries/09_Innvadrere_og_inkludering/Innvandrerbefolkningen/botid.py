525c21ec82490f07ba2a6a9283932b1514542b7a
Even Sannes Riiser <59871903+evensrii@users.noreply.github.com>
2024-12-05 15:40:44 +0100

    dfg

--- Python/Queries/09_Innvadrere_og_inkludering/Innvandrerbefolkningen/botid.py
+++ Python/Queries/09_Innvadrere_og_inkludering/Innvandrerbefolkningen/botid.py
@@ -25,7 +25,7 @@ error_messages = []
 # Jeg bruker requests for å simulere nedlasting av filen.
 
 # Finner URL vha. "Inspiser side" og fane "Network" (F12)
-url = "https://app-simapi-prod.azurewebsites.net/download_csv/k/befolkning_botid"
+url = "https://app-simapi-prod.azurewebsites.net/download_csv/k/befolkning_boti"
 
 ## Kjøre spørringer i try-except for å fange opp feil. Quitter hvis feil.
 
