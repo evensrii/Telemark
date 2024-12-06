2cf750d88e31e4c760d94c6a83e399179c912f07
Even Sannes Riiser <59871903+evensrii@users.noreply.github.com>
2024-12-06 13:49:46 +0100

    asd

--- Python/Automatisering/Task scheduler/scheduled_scripts.bat
+++ Python/Automatisering/Task scheduler/scheduled_scripts.bat
@@ -31,11 +31,37 @@ CALL conda activate analyse >> %LOGFILE% 2>&1
 :: Run scripts with log separation
 
 :: Innvandrere og inkludering
-CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_innvandrere.py" "Innvandrere - Andel sysselsatte"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\innvandrere_bosatt.py" "Innvandrere - Bosatt"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\innvandringsgrunn.py" "Innvandrere - Innvandringsgrunn"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_flyktninger_og_arbeidsinnvandrere.py" "Innvandrere - Flyktninger og arbeidsinnvandrere"
 CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Innvandrerbefolkningen\botid.py" "Innvandrere - Botid"
+
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Bosetting_av_flyktninger\anmodninger_og_faktisk_bosetting.py" "Innvandrere - Anmodninger og faktisk bosetting"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Bosetting_av_flyktninger\enslige_mindreårige.py" "Innvandrere - Enslige mindreårige"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Bosetting_av_flyktninger\botid.py" "Innvandrere - Sekundærflytting"
+
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Introduksjonsprogrammet\deltakere_introduksjonsprogram.py" "Innvandrere - Deltakere introdukjonsprogrammet"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Introduksjonsprogrammet\etter_introduksjonsprogram.py" "Innvandrere - Etter introduksjonsprogrammet"
+
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Utdanning\minoriteter_barnehage.py" "Innvandrere - Minoriteter i barnehage"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Utdanning\innv_fullført_vgo.py" "Innvandrere - Fullført VGO"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Utdanning\innv_høyeste_utdanning.py" "Innvandrere - Høyeste utdanning"
+
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_innvandrere.py" "Innvandrere - Sysselsatte"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_sysselsatte_etter_botid_og_landbakgrunn.py" "Innvandrere - Sysselsatte etter botid og bakgrunn"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\09_Innvadrere_og_inkludering\Arbeid_og_inntekt\andel_innvandrere_i_lavinntekt.py" "Innvandrere - Lavinntekt"
+
+:: Klima og energi
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\04_Klima_og_energi\Klimagassutslipp\klimagassutslipp.py" "Klima og energi - Sektorvise utslipp"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\04_Klima_og_energi\Klimagassutslipp\norskeutslipp.py" "Klima og energi - Utslipp fra landbasert industri"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\04_Klima_og_energi\Ressursforvaltning\økologisk_tilstand.py" "Klima og energi - Økologisk tilstand vann"
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\04_Klima_og_energi\Ressursforvaltning\antall_felt.py" "Klima og energi - Felte hjortedyr"
 
 :: Areal og stedsutvikling
 CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\10_Areal_og_stedsutvikling\Areal_til_jordbruk\jordbruksareal_per_kommune.py" "Areal - Jordbruksareal per kommune"
+
+:: Idrett, friluftsliv og frivillighet
+CALL :RunScript "D:\Scripts\analyse\Telemark\Python\Queries\07_Idrett_friluftsliv_og_frivillighet\Friluftsliv\andel_jegere.py" "Idrett, friluftsliv og frivillighet - Jegere"
 
 :: Run the email script and log its output
 python -u "D:\Scripts\analyse\Telemark\Python\Automatisering\Task scheduler\email_when_run_completed.py" > %EMAIL_LOG% 2>&1
