3747ef0965e821f4df8926bf46e985aaca4f21c2
Even Sannes Riiser <even.s.riiser@gmail.com>
2025-08-14 13:27:28 +0200

    asd

--- Python/Queries/09_Innvandrere_og_inkludering/Utdanning/minoriteter_barnehage.py
+++ Python/Queries/09_Innvandrere_og_inkludering/Utdanning/minoriteter_barnehage.py
@@ -12,7 +12,6 @@ import numpy as np
 from Helper_scripts.utility_functions import fetch_data, delete_files_in_temp_folder
 from Helper_scripts.email_functions import notify_errors
 from Helper_scripts.github_functions import upload_github_file, download_github_file, compare_to_github, handle_output_data
-from Helper_scripts.utility_functions import notify_errors
 
 # Capture the name of the current script
 script_name = os.path.basename(__file__)
